6c75c6c3ef197a01459e1dd955d5f11e7007cec5
mateoghidini1998 <mateghidini123@gmail.com>
2024-05-16 22:55:55 -0300

    Add icons components

---
+++ src/components/icons/Penal.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Penal() {
+  return (
+    <svg width="40" height="40" viewBox="0 0 53 56" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <circle cx="11.7778" cy="43.9001" r="10.2778" stroke="#4f46e5" stroke-width="3"/>
+    <circle cx="41.2221" cy="20.3444" r="10.2778" stroke="#4f46e5" stroke-width="3"/>
+    <rect x="8.48052" y="11.891" width="11.1643" height="6.22766" rx="3.11383" transform="rotate(-44.3122 8.48052 11.891)" stroke="#4f46e5" stroke-width="2"/>
+    <rect x="22.9344" y="1.41938" width="11.1643" height="6.22766" rx="3.11383" transform="rotate(5.87562 22.9344 1.41938)" stroke="#4f46e5" stroke-width="2"/>
+    <rect x="3.55053" y="28.8983" width="11.1643" height="6.22766" rx="3.11383" transform="rotate(-77.3245 3.55053 28.8983)" stroke="#4f46e5" stroke-width="2"/>
+    <line x1="12.3026" y1="13.4132" x2="9.55707" y2="19.8193" stroke="#4f46e5" stroke-width="2" stroke-linecap="round"/>
+    <line x1="8.2172" y1="28.2331" x2="9.06618" y2="33.327" stroke="#4f46e5" stroke-width="2" stroke-linecap="round"/>
+    <line x1="24.0466" y1="5.09196" x2="17.7252" y2="6.89807" stroke="#4f46e5" stroke-width="2" stroke-linecap="round"/>
+    <line x1="35.1052" y1="10.7662" x2="31.6435" y2="6.43904" stroke="#4f46e5" stroke-width="2" stroke-linecap="round"/>
+    </svg>
+
+  )
+}
