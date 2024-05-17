e4a8395231911d3cd0343e716998886612d025c2
mateoghidini1998 <mateghidini123@gmail.com>
2024-05-16 22:56:18 -0300

    Modify page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
+import { AboutSection } from "@/components/about/AboutSection";
+import HomeSection from "@/components/home/HomeSection";
 import Header from "@/components/layout/Header";
-import Image from "next/image";
+import ServicesSection from "@/components/services/ServicesSection";
+import Team from "@/components/team/Team";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Header/>
+    <main className="flex pl-12 min-h-screen flex-col items-center justify-between">
+      <Header />
+      <HomeSection/>
+      <AboutSection/>
+      <Team/>
+      <ServicesSection/>
     </main>
   );
 }
