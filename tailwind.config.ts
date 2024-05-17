bb82b34910b95f07b8ddec89b2fbd88443df335b
mateoghidini1998 <mateghidini123@gmail.com>
2024-05-16 22:55:08 -0300

    add custom font size

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,9 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      fontSize: {
+        "5xl": "3.75rem",
+      }
     },
   },
   plugins: [],
