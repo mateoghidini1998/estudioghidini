ea599e92b25ba1345191cdece70c2ae4f1945237
mateoghidini1998 <mateghidini123@gmail.com>
2024-05-16 22:56:02 -0300

    Modify Header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,19 +1,20 @@
-import Image from "next/image"
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="w-full h-10 flex items-center justify-between">
-        <div>
-            <Image src="/favicon.ico" alt="Logo" width={50} height={50} />
-        </div>
-        <nav>
-            <ul className="flex gap-4 items-center">
-                <li>Home</li>
-                <li>About us</li>
-                <li>Services</li>
-                <li>Contact</li>
-            </ul>
-        </nav>
+    <header className="w-full fixed top-0 left-0 right-0 h-14 flex items-center px-6 gap-4 z-50">
+      <div className="flex items-center h-full mt-2 ">
+        <Image src="/logo.png" alt="Logo" width={40} height={40} />
+      </div>
+      <nav>
+        <ul className="flex gap-4 items-center">
+          <li><Link href="#">Inicio</Link></li>
+          <li><Link href="#">Sobre Nosotros</Link></li>
+          <li><Link href="#">Servicios</Link></li>
+          <li><Link href="#">Contacto</Link></li>
+        </ul>
+      </nav>
     </header>
-  )
+  );
 }
